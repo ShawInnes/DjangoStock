92149fd3304a84ec069379d5728296cd2140d8f9
Shaw Innes <shaw@immortal.net.au>
2019-11-17 10:15:12 +1000

    Add API Responses

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    api_request = requests.get('https://cloud.iexapis.com/stable/stock/aapl/quote?tok')
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
