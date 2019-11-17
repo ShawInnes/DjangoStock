05a50347229e1dd94080e68a6695b480a299ce03
Shaw Innes <shaw@immortal.net.au>
2019-11-17 12:06:10 +1000

    Add Stock Model

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -13,15 +13,25 @@ def home(request):
     import requests
     import json
 
-    api_request = requests.get(f'https://cloud.iexapis.com/stable/stock/aapl/quote?token=' + api_key)
+    if request.method == "POST":
+        ticker = request.POST['ticker']
+        api_request = requests.get(f'https://cloud.iexapis.com/stable/stock/{ticker}/quote?token={api_key}')
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'home.html', {'api': api})
 
-    return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'ticker': 'Enter a ticker above...'})
+
+    return render(request, 'home.html', {})
 
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def add_stock(request):
+    return render(request, 'add_stock.html', {})
